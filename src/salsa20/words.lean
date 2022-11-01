2cab45a3ce1c7f8df1eedf30efe0348cb29f9fef
Alfredo Garcia <oxarbitrage@gmail.com>
2022-11-01 14:53:08 -0300

    add basic operation inverses

--- src/salsa20/words.lean
+++ src/salsa20/words.lean
@@ -32,8 +32,10 @@ def mod : ℕ := (2^word_len)
 def bitvec_as_mod (v : bitvec word_len) : zmod mod := v.to_nat
 
 -- convert two bitvec to the sum of them modulo 2^32
-def sum_as_mod (u v : bitvec word_len) : zmod mod := 
-  bitvec_as_mod u + bitvec_as_mod v
+def add_bitvecs_as_mod (u v : bitvec word_len) : zmod mod := bitvec_as_mod u + bitvec_as_mod v
+
+-- convert two bitvec to the rest of them modulo 2^32
+def substract_bitvecs_as_mod (u v : bitvec word_len) : zmod mod := bitvec_as_mod u - bitvec_as_mod v
 
 -- convert a mod 2^32 to a nat
 def mod_as_nat (sum : zmod mod) : ℕ := sum.val
@@ -41,12 +43,15 @@ def mod_as_nat (sum : zmod mod) : ℕ := sum.val
 -- convert a nat to a bitvec
 def nat_as_bitvec (n : ℕ) : bitvec word_len := bitvec.of_nat word_len n
 
+-- the inverse of the zmod sum of 2 bitvectors is the substraction of them
+def zmod_addition_inverse (a : zmod mod) : ℕ := mod - a.val  
+
 -- xor
 
 -- axioms
 
 -- a ⊕ a = 0
-axiom xor_twice_eq_zero (a : bitvec word_len) : a.xor a = bitvec.zero word_len
+axiom xor_equals_eq_zero (a : bitvec word_len) : a.xor a = bitvec.zero word_len
 -- a ⊕ 0 = a
 axiom xor_zero (a : bitvec word_len) : a.xor (bitvec.zero word_len) = a
 -- (a ⊕ b) ⊕ b = a
@@ -54,7 +59,13 @@ axiom xor_assoc_v1 (a b : bitvec word_len) : (a.xor b).xor b = a
 -- (a ⊕ b) ⊕ a = b
 axiom xor_assoc_v2 (a b : bitvec word_len) : (a.xor b).xor a = b
 
--- theorem
+-- theorems
+
+-- applying the same xor_by twice reverses to the original value 
+lemma xor_twice_reverses (orig xor_by : bitvec word_len) : orig = (orig.xor xor_by).xor xor_by :=
+begin
+  rw xor_assoc_v1,
+end
 
 -- c = a ⊕ b ↔ (a = c ⊕ b) ∧ (b = c ⊕ a)
 theorem xor_is_its_own_inverse (a b c : bitvec word_len) : c = a.xor b ↔ (a = c.xor b) ∧ (b = c.xor a) :=
@@ -85,5 +96,21 @@ end
 -- Implements DJB's definition of '<<<' : https://github.com/alexwebr/salsa20/blob/master/salsa20.c#L6
 def rotl (input: bitvec 32) (shift : fin 32) : bitvec 32 :=
   (input.shl shift).or (input.ushr (32 - shift))
+
+-- Inverse of rotl
+def rotl_inv (input: bitvec 32) (shift : fin 32) : bitvec 32 :=
+  (input.ushr shift).or (input.shl (32 - shift))
+
+-- theorems
+
+-- the inverse always undoes rotl 
+theorem rotl_inv_undoes_rotl (input: bitvec 32) (shift : fin 32) : 
+  input = rotl_inv (rotl input shift) shift := 
+begin
+  unfold rotl,
+  unfold rotl_inv,
+  sorry,
+end
+
 
 end words
