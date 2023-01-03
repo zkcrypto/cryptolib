5ed249d9b81d0a2a756afc7bfc61bc9a5ecb9d26
Ashley Blacquiere <ashley.blacquiere@protonmail.com>
2023-01-03 06:55:24 -0800

    move commitments from scratch to src

--- src/commitments.lean
+++ src/commitments.lean
@@ -17,10 +17,12 @@ noncomputable theory
   D = space of opening values (order q)
   C = commitment space (in practice includes committed message and opening value computed by S)
 -/
-variables {G M R C : Type} [decidable_eq M]
+variables {G M R C A_state : Type} [decidable_eq M]
           (gen : pmf G)
           (commit : G → M → pmf C)
           (verify : G → C → M)
+          (A1 : G → pmf (M × M × A_state)) -- Rewrite
+          (A2 : C → A_state → pmf (zmod 2)) -- Rewrite
 
 /- 
   Executes commitment protocol defined by opening_value, commit and verify
@@ -29,7 +31,7 @@ variables {G M R C : Type} [decidable_eq M]
 -- `Verify(c, m, d)` holds
 def commit_verify (m : M) : pmf (zmod 2) := 
 do 
-  h ← gen, -- Not needed? h isn't typical of all commitment schemes - gen should generate G, g, q, and h in the case of Pedersen and elGamal
+  h ← gen, -- Not needed? h isn't typical of all commitment schemes - gen should generate G, g, q, and h in the case of Pedersen and elgamal
   d ← gen, -- But this should be chosen uniformly from ℤq
   c ← commit h m, -- c would be some (G x ℤq) - opening value as part of commit
   pure (if verify h c = m then 1 else 0) 
@@ -50,9 +52,9 @@ def commitment_correctness : Prop := ∀ (m : M), commit_verify gen commit verif
   Binding: "no adversary (either powerful or computationally bounded) can generate c, m 6 = m′ and d, d′ such that both Verify(c, m, d) and Verify(c, m′, d′) accept"
 -/
 
-def hiding 
 
-def binding
+def hiding_prop : Prop := sorry -- Note can't use 'hiding' for some reason - namespace collision?
+def binding_prop : Prop := sorry
 
 
 
@@ -64,7 +66,7 @@ def binding
 -/
 def HG : pmf (zmod 2):= 
 do 
-  k ← keygen,  -- No need for this
+  -- k ← keygen,  -- No need for this
   m ← A1 k.1, -- A1 outputs M
   b ← uniform_2, -- adversary only gets first component of pair
   c ← encrypt k.1 (if b = 0 then m.1 else m.2.1),
@@ -72,7 +74,7 @@ do
   pure (1 + b + b')
 
 -- SSG(A) denotes the event that A wins the semantic security game
-local notation `Pr[SSG(A)]` := (SSG keygen encrypt A1 A2 1 : ℝ)
+local notation `Pr[HG(A)]` := (HG keygen encrypt A1 A2 1 : ℝ) -- Rewrite
 
 def hiding_property (ε : nnreal) : Prop := abs (Pr[HG(A)] - 1/2) ≤ ε 
 -- Also need perfect hiding
