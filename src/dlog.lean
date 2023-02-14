f19dd92fa9db662f052013c57fed37db764fb2f0
ashandoak <ashley.blacquiere@protonmail.com>
2023-02-14 08:28:57 -0800

    add dlog.lean

--- src/dlog.lean
+++ src/dlog.lean
@@ -3,6 +3,8 @@ import uniform
 
 noncomputable theory 
 
+section DL
+
 variables (G : Type) [fintype G] [group G]
           (g : G) (g_gen_G : ∀ (x : G), x ∈ subgroup.gpowers g)
           (q : ℕ) [fact (0 < q)] (G_card_q : fintype.card G = q) 
@@ -12,15 +14,13 @@ include g_gen_G G_card_q
 
 def DLG : pmf (zmod q) := 
 do 
-  α ← uniform_zmod q, 
-  α' ← A (g^α.val),
+  α ← uniform_zmod q,
+  α' ←  A (g^α.val),
   pure α'
 
-def DLG' : pmf (zmod q) × pmf (zmod q) := 
-do 
-  α ← uniform_zmod q, 
-  α' ← A (g^α.val),
-  pure (α × α')
+def DLadv : Prop := ((DLG G g g_gen_G q G_card_q A).1 1 : ℝ) = ((DLG G g g_gen_G q G_card_q A).2 1 : ℝ)
 
 
-def DLadv : Prop := (DLG' G g g_gen_G q G_card_q A).1 = (DLG' G g g_gen_G q G_card_q A).2
+#check DLG G g g_gen_G q G_card_q A 
+
+end DL
