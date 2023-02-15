715cafa8437c82298da630e08022443721c34c70
ashandoak <ashley.blacquiere@protonmail.com>
2023-02-14 22:08:17 -0800

    use group G rather than zmod q

--- src/dlog.lean
+++ src/dlog.lean
@@ -6,21 +6,30 @@ noncomputable theory
 section DL
 
 variables (G : Type) [fintype G] [group G]
-          (g : G) (g_gen_G : ∀ (x : G), x ∈ subgroup.gpowers g)
+          (g : G) (g_gen_G : ∀ (x : G), x ∈ subgroup.gpowers g) 
           (q : ℕ) [fact (0 < q)] (G_card_q : fintype.card G = q) 
-          (A : G → pmf (zmod q))
+          (A : G → pmf G)
 
-include g_gen_G G_card_q
+include g_gen_G G_card_q -- assumptions used in the game reduction
 
-def DLG : pmf (zmod q) := 
+def DL : pmf G := 
 do 
   α ← uniform_zmod q,
-  α' ←  A (g^α.val),
+  let u := g^α.val,
+  α' ←  A u,
   pure α'
 
-def DLadv : Prop := ((DLG G g g_gen_G q G_card_q A).1 1 : ℝ) = ((DLG G g g_gen_G q G_card_q A).2 1 : ℝ)
+-- -- From DDH:
+-- -- DDH0(D) is the event that D outputs 1 upon receiving (g^x, g^y, g^(xy))
+-- local notation `Pr[DDH0(D)]` := (DDH0 G g g_gen_G q G_card_q D 1 : ℝ)
+
+-- -- DDH1(D) is the event that D outputs 1 upon receiving (g^x, g^y, g^z)
+-- local notation `Pr[DDH1(D)]` := (DDH1 G g g_gen_G q G_card_q D 1 : ℝ)
 
 
-#check DLG G g g_gen_G q G_card_q A 
+def DLadv : Prop := sorry
+
+
+#check DL G g g_gen_G q G_card_q A 
 
 end DL
