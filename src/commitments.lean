ab3a0116e48282946e2e21e81bfc0cdf938b3336
ashandoak <ashley.blacquiere@protonmail.com>
2023-03-08 14:53:58 -0800

    modify verify and binding adversary
    added some meeting notes for future changes

--- src/commitments.lean
+++ src/commitments.lean
@@ -33,10 +33,10 @@ From Boneh & Shoup:
 A security parameter (λ) and system parameter (Λ) are used to index families of key spaces, message spaces and ciphertext spaces. 
 -/
 
-variables {G M D C A_state : Type}
+variables {G M D C A_state : Type} [decidable_eq M]
           (gen : pmf G) -- generates the public parameter, h ∈ G
           (commit : G → M → pmf (C × D) )
-          (verify : G → C → D → M → bool)
+          (verify : G → C → D → M → zmod 2)
           (BindingAdversary : G → pmf (C × D × D × M × M)) -- how to ensure these are two different Ms?
           (HidingAdversary1 : G → pmf (M × M × A_state)) -- double check how Lupo handles state
           (HidingAdversary2 : G → C → A_state → pmf (zmod 2) )
@@ -52,7 +52,7 @@ def commit_verify (m : M) : pmf (zmod 2) := -- formerly included a (d : D) param
 do 
   h ← gen, 
   c ← commit h m, 
-  pure (if verify h c.1 c.2 m then 1 else 0) --c.2 is the opening value
+  pure (if verify h c.1 c.2 m = 1 then 1 else 0) --c.2 is the opening value
 
 /- 
   A commitment protocol is correct if verification undoes 
@@ -74,11 +74,16 @@ def commitment_correctness : Prop := ∀ (m : M), commit_verify gen commit verif
 def BG : pmf (zmod 2) :=
 do 
   h ← gen, 
-  bc ← BindingAdversary h,
+  bc ← BindingAdversary h, --pmf (C × D × D × M × M)
   -- Def. of binding in B&S pg. 337
-  -- The then else results have been reversed from expectation to account for needing a negation (need to make this clearer)
   -- As per comment above - how to ensure the Ms are unique?
-  pure (if verify h bc.1 bc.2.1 bc.2.2.2.1 = verify h bc.1 bc.2.2.1 bc.2.2.2.2 then 1 else 0)
+  -- Verify that both return 1
+  -- Commitments are valid commitments to a message
+  let b := verify h bc.1 bc.2.1 bc.2.2.2.1,
+  let b' := verify h bc.1 bc.2.2.1 bc.2.2.2.2,
+  -- let b'' := (if bc.2.2.2.1 = bc.2.2.2.2 
+  pure (if bc.2.2.2.1 = bc.2.2.2.2 then 0 else b * b')
+  
 local notation `Pr[BG(A)]` := (BG gen verify BindingAdversary 1 : ℝ)
 
 def computational_binding_property (ε : nnreal) : Prop := abs (Pr[BG(A)] - 1/2) ≤ ε -- the 1/2 term is necessary here?
@@ -95,15 +100,26 @@ def computational_binding_property (ε : nnreal) : Prop := abs (Pr[BG(A)] - 1/2)
 -/
 -- Split into two phases: 1. return two M; 2. return bit
 
-def docommit (m : M) : pmf (C × D) :=
+def docommit (m : M) : pmf C :=
 do
   h ← gen,
   c ← commit h m, 
-  pure c
+  pure c.1 -- return just the commit, not the opening value
 
+
+-- Perfect hiding strategy: Use uniformity prop. of group to replace the commit with something completely random
+-- Adv with no knowledge of the message can't guess the message with greater prob than 1/|M|
+-- Any adv that wins the perf. hiding game also wins the message guessing game - contradiction
+-- Breaking the perfect hiding game breaks the impossible message game
+-- Perf. hiding as equality between pmfs ∀m1,m2 
+-- Pedersen commitments are uniform so equivalence shows 
+
+def perfect_hiding_property : Prop := ∀ (m1 m2 : M), docommit gen commit m1 = docommit gen commit m2 -- This is an equality between distributions - how is this proved?
+
+#check docommit
 def HG : pmf (zmod 2) := 
 do 
-  hc ← HidingAdversary,
+  hc ← HidingAdversary1,
   b ← uniform_2,
   c ← commit hc.b,
   let b' := A c,
@@ -113,7 +129,7 @@ local `Pr[HG(A)]` := (HG verify HidingAdversary 1 : ℝ)
 
 #check HG commit HidingAdversary A 
 
-def hiding_property (ε : nnreal) : Prop := abs (Pr[HG(A)] - 1/2) ≤ ε
+def computational_hiding_property (ε : nnreal) : Prop := abs (Pr[HG(A)] - 1/2) ≤ ε
 
 -- game where adv. generates two messages
 -- commiter commits to one chosen at random
